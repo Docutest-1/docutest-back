0aea3914ff78083d2f24c7f3a749ca5d4194b3f7
Nawlej <churknam@gmail.com>
2020-09-30 08:56:53 -0700

    Added controllerTest

---
+++ src/test/java/com/revature/docutest/ControllerTest.java
@@ -0,0 +1,43 @@
+package com.revature.docutest;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ControllerTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        
+    }
+    
+    @AfterAll
+    static void afterAll() {
+        
+    }
+    
+    @BeforeEach
+    static void beforeEach() {
+        
+    }
+    
+    @AfterEach
+    static void afterEach() {
+        
+    }
+    
+    @Test
+    void readFileTest() {
+        
+    }
+    
+    @Test
+    void uploadFileTest() {
+        
+    }
+
+}
