cc7a81eabbce588437b4c2193626ed7106993d22
pl9ed <pl9ed@virginia.edu>
2020-10-09 13:44:26 -0400

    Added functionality for path and header parameters to Request object

--- src/test/java/com/revature/services/JMeterServiceTest.java
+++ src/test/java/com/revature/services/JMeterServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -25,14 +27,10 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.when;
-
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.revature.docutest.DocutestApplication;
 import com.revature.docutest.TestUtil;
@@ -40,9 +38,6 @@ import com.revature.models.SwaggerDocutest;
 import com.revature.models.SwaggerSummary;
 import com.revature.templates.LoadTestConfig;
 
-import io.swagger.models.HttpMethod;
-import io.swagger.models.Operation;
-import io.swagger.models.Path;
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
 
@@ -60,7 +55,8 @@ class JMeterServiceTest {
     @Mock
     private SwaggerSummaryService sss;
     
-    private static OASService adapter = new OASService(new JSONStringCreator());
+    @Autowired
+    private static OASService adapter;
 
     private SwaggerDocutest testSpecs;
     
@@ -211,11 +207,10 @@ class JMeterServiceTest {
         expected.clear();
         expected.add("/todos");
         expected.add("/todos/truncate");
-        expected.add("/todos/{id}"); // TODO needs to be changed once path var is implemented
+        expected.add("/todos/1"); 
         samplers = jm.createHTTPSamplerProxy(testSpecs);
 
         for (HTTPSamplerProxy sampler : samplers) {
-            System.out.println(sampler.getPath());
             assertTrue(expected.contains(sampler.getPath()));
         }
     }
@@ -285,7 +280,6 @@ class JMeterServiceTest {
             String bodyText = argsMap.get("");            
 
             if (!argsMap.isEmpty()) {
-                System.out.println(bodyText);
                 // argsMap returns a string with "null" if no valid values
                 assertNotEquals("null",bodyText);
             } else {
